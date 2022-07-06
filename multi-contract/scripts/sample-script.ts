7b8425cbe062c4899dd201f80c7f25a0e14d7a33
udit-gulati <uditgulati6@gmail.com>
2022-07-07 01:17:32 +0530

    convert multiple contracts scripts to ts

--- multi-contract/scripts/sample-script.ts
+++ multi-contract/scripts/sample-script.ts
@@ -1,15 +1,16 @@
-const { Contract, getAccountByName, getLogs } = require("junokit");
+import { getAccountByName } from "junokit";
+
+import { CounterContract } from "../artifacts/typescript_schema/Counter";
+import { FactorialContract } from "../artifacts/typescript_schema/Factorial";
 
 async function run() {
   const runTs = String(new Date());
   const contract_owner = getAccountByName("account_0");
   const other = getAccountByName("account_1");
-  const counter_contract = new Contract("counter");
-  const factorial_contract = new Contract("factorial");
+  const counter_contract = new CounterContract;
+  const factorial_contract = new FactorialContract;
   await counter_contract.setUpclient();
   await factorial_contract.setUpclient();
-  await counter_contract.parseSchema();
-  await factorial_contract.parseSchema();
 
   console.log("Client setup done!! ");
 
@@ -41,16 +42,16 @@ async function run() {
     }, `deploy test ${runTs}`, contract_owner);
   console.log(contract_inf);
 
-  const inc_respons = await factorial_contract.tx.factorial(
+  const inc_respons = await factorial_contract.factorial(
     {account: contract_owner},
     {
       "number": 4
     });
   console.log(inc_respons);
-  const inc_response = await counter_contract.tx.increment({account: contract_owner});
+  const inc_response = await counter_contract.increment({account: contract_owner});
   console.log(inc_response);
 
-  const response = await counter_contract.query.get_count();
+  const response = await counter_contract.getCount();
   console.log(response);
 
 }
