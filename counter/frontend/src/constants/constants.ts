a93682f2f8993e88253c369cac0aca9b0e60386a
udit-gulati <uditgulati6@gmail.com>
2023-04-17 22:31:54 +0530

    remove frontend and lock file

--- counter/frontend/src/constants/constants.ts
+++
@@ -1,1 +0,0 @@
-export const API_URL = 'https://rpc.uni.junonetwork.io/';
--- counter/frontend/src/contracts/contactAddress.ts
+++
@@ -1,4 +0,0 @@
-export const contractAddress = {
-    at:'juno1kkwhav7ag06t6xfsrwl2pzu44kaqnrh38ea6t5jv7zan7zy6wlgqlahf32',
-    id:'contractAddress'
-}
